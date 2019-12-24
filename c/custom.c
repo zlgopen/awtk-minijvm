8efee368c517370beb7d8aeaaf84e9691bbece9f
xianjimli <xianjimli@gmail.com>
2019-12-24 16:20:18 +0800

    add assets

--- c/custom.c
+++ c/custom.c
@@ -1,3 +1,6 @@
+
+#define WITH_FS_RES 1
+
 int awtk_TEmitter_emitter_on(Runtime *runtime, JClass *clazz) {
   /*TODO*/
   return 0;
@@ -23,6 +26,8 @@ int awtk_TWidget_widget_foreach(Runtime *runtime, JClass *clazz) {
   return 0;
 }
 
+#include "assets.c"
+
 int awtk_AWTK_init(Runtime *runtime, JClass *clazz) {
   jni_ctx_t ctx = jni_ctx_init(runtime, clazz);
 
@@ -30,7 +35,9 @@ int awtk_AWTK_init(Runtime *runtime, JClass *clazz) {
   uint32_t h = (uint32_t)jni_ctx_get_int(&ctx);
 
   ret_t ret = tk_init(w, h, APP_SIMULATOR, "AWTK-JAVA", NULL);
-  
+
+  assets_init();
+  tk_ext_widgets_init();  
   jni_ctx_return_int(&ctx, (int32_t)(ret));
 
   return 0;
